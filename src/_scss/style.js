151152419e759c60a7a6e3294455499b4ac09447
ttdat4 <dh51702456@student.stu.edu.vn>
2022-07-12 23:53:51 +0700

    update

---
+++ src/_scss/style.js
@@ -0,0 +1,25 @@
+import './style.scss'
+
+
+// var scrollPos =0;
+// $(window).scroll(function () {
+// //     var curscrollPos = $(this).scrollTop();
+// //     if (curscrollPos < scrollPos) 
+// //     {
+// //       $(".c-mainvisual__cotton").css("transform", "tranlateY(0%)");
+// //     //   $(".c-mainvisual__cotton").css({
+// //     //     //"transform": "tranlateY()",
+// //     //     "animation": "downScroll ease-in-out 2s infinite alternate",
+// //     //     });
+// //     } 
+// //   else {
+// //     $(".c-mainvisual__cotton").css({
+// //         "transform": "tranlateY(-330%)",
+// //         "animation": "downScroll ease-in-out 2s infinite alternate",
+// //     });
+// //   }
+// //   scrollPos = curscrollPos;
+// console.log("Dat Dep Trai");
+//   });
+  
+  
